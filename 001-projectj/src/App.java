791452a630ccd889bcaf41ac36b2f384c8df4620
Jeferson Lopes <jefersonlopes.br@gmail.com>
2024-02-05 22:16:26 -0300

    Adicionei 'wow' no 'Hello world'

--- 001-projectj/src/App.java
+++ 001-projectj/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String myVar = "Hello, Welcome to the jungle!";
+        String myVar = "Hello, Welcome to the jungle! Wow";
         System.out.println(myVar);
     }
 }
